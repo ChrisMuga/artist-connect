f75598a73b978572606a2fd21b42c66480c59447
chris <chrismuga94@gmail.com>
2018-09-01 02:20:16 +0300

    typeahead js

--- resources/views/users/typeahead.blade.php
+++ resources/views/users/typeahead.blade.php
@@ -9,20 +9,30 @@
 <script>
 var data;
 
-//fetch data
-fetch('fetch')
-  .then(function(response) 
-  {
-    return response.json();
-  })
-  .then(function(myJson) 
-  
-  {
-    
-      data  = myJson[0].name;
-      console.log(data);
-   
-  });
+
+  var countries = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  // url points to a json file that contains an array of country names, see
+  // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
+  prefetch: 'fetch'
+});
+
+console.log(countries);
+
+$('#search').typeahead(null, {
+  name: 'countries',
+  display: 'name',
+  source: countries,
+  templates: {
+    empty: [
+      '<div class="empty-message">',
+        'unable to find any Best Picture winners that match the current query',
+      '</div>'
+    ].join('\n'),
+    suggestion: Handlebars.compile('<div><strong>'+countries+'</strong> – '+countries+'</div>')
+  }
+});
 
   
 </script>
